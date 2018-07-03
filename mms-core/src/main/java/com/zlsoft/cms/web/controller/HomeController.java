42438de0dc921f58b0e2179a3811c7cfa2df22f8
waonbin <waonbin@126.com>
2018-07-03 22:14:39 +0800

    init project

---
+++ mms-core/src/main/java/com/zlsoft/cms/web/controller/HomeController.java
@@ -0,0 +1,58 @@
+package com.zlsoft.cms.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "member/login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "member/register";
+    }
+
+    @RequestMapping("/register2")
+    public String register2() {
+        return "member/register2";
+    }
+
+    @RequestMapping("/register3")
+    public String register3() {
+        return "member/register3";
+    }
+
+    @RequestMapping("/edit")
+    public String edit() {
+        return "member/personal_edit";
+    }
+
+    @RequestMapping("/info")
+    public String info() {
+        return "member/personal_information";
+    }
+
+    @RequestMapping("/pay")
+    public String pay() {
+        return "member/personal_pay";
+    }
+
+    @RequestMapping("/payment")
+    public String payment() {
+        return "member/personal_payment";
+    }
+
+    @RequestMapping("/revise")
+    public String revise() {
+        return "member/personal_revise";
+    }
+}
