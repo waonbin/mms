36fcd2b4da9f6b900e3af74c9c914de1dad0ee66
waonbin <waonbin@126.com>
2018-07-07 12:07:20 +0800

    fix logout

--- mms-portal/src/main/java/com/zlsoft/portal/web/controller/LoginController.java
+++ mms-portal/src/main/java/com/zlsoft/portal/web/controller/LoginController.java
@@ -69,6 +69,6 @@ public class LoginController {
     @RequestMapping(value = "logout", method = RequestMethod.GET)
     public String logout(HttpSession session){
         session.removeAttribute(Constants.SESSION_USER);
-        return "/index";
+        return "/login";
     }
 }
