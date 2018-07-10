0acf04eaaf8685e2f5838bafd7f73e63129f799b
waonbin <waonbin@126.com>
2018-07-10 20:22:06 +0800

    change interface for member

--- mms-manager/src/main/java/com/zlsoft/manager/web/controller/MemberController.java
+++ mms-manager/src/main/java/com/zlsoft/manager/web/controller/MemberController.java
@@ -5,7 +5,7 @@ import com.zlsoft.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -22,23 +22,38 @@ public class MemberController {
      * @return individual members' list page
      */
     @GetMapping("/individual_members")
-    public ModelAndView individualMembers() {
-        List<Member> members = this.memberService.findAll();
-        ModelAndView mav = new ModelAndView("/admin/member/individual_members");
-        mav.addObject("members", members);
-        return mav;
+    public String individualMembers() {
+        return "/admin/member/individual_members";
     }
 
     /**
-     * GET  /personal_information : get group members' list page
+     * GET  /personal_information/data : get data of individual members' list page
+     * @return data of individual members' list page
+     */
+    @GetMapping("/individual_members/data")
+    public @ResponseBody List<Member>
+    getIndividualMembers() {
+        List<Member> members = this.memberService.findAll();
+        return members;
+    }
+
+    /**
+     * GET  /group_members : get group members' list page
      * @return group members' list page
      */
     @GetMapping("/group_members")
-    public ModelAndView groupMembers() {
+    public String groupMembers() {
+        return "/admin/member/group_members";
+    }
+
+    /**
+     * GET  /group_members/data : get data of group members' list page
+     * @return data of group members' list page
+     */
+    @GetMapping("/group_members/data")
+    public @ResponseBody List<Member> getGroupMembers() {
         List<Member> members = this.memberService.findAll();
-        ModelAndView mav = new ModelAndView("/admin/member/group_members");
-        mav.addObject("members", members);
-        return mav;
+        return members;
     }
 
     /**
@@ -46,10 +61,17 @@ public class MemberController {
      * @return group members' list page
      */
     @GetMapping("/register_check")
-    public ModelAndView registerCheck() {
+    public String registerCheck() {
+        return "/admin/member/register_check";
+    }
+
+    /**
+     * GET  /register_check/data : get data of group members' list page
+     * @return data of group members' list page
+     */
+    @GetMapping("/register_check/data")
+    public @ResponseBody List<Member> getRegisterCheck() {
         List<Member> members = this.memberService.findAll();
-        ModelAndView mav = new ModelAndView("/admin/member/register_check");
-        mav.addObject("members", members);
-        return mav;
+        return members;
     }
 }
