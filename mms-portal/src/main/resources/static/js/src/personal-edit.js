941892210b78776cbc2d4513ef12e298b93de340
zhangwei <546052344@qq.com>
2018-07-11 23:17:45 +0800

    用户编辑

--- mms-portal/src/main/resources/static/js/src/personal-edit.js
+++ mms-portal/src/main/resources/static/js/src/personal-edit.js
@@ -4,83 +4,80 @@ $(function() {
        data: function() {
            var _this = this;
 
-           var validatePass = function(rule, value, callback) {
-               if (value === '') {
-                   callback(new Error('请输入密码'));
-               } else if(value.length >= 8 && value.length <= 20) {
-                   if (_this.registerValidateForm.checkPass !== '') {
-                       _this.$refs.registerValidateForm.validateField('checkPass');
-                   }
-                   callback();
-               } else {
-                   callback(new Error('请输入8到20位密码'));
-               }
-           };
-           var validatePass2 = function(rule, value, callback) {
-               if (value === '') {
-                   callback(new Error('请再次输入密码'));
-               } else if (value !== _this.registerValidateForm.pass) {
-                   callback(new Error('两次输入密码不一致!'));
-               } else {
-                   callback();
-               }
-           };
-           var validatePhone = function(rule, value, callback) {
-               if(value.length === 0) {
-                   callback(new Error('请输入手机号码'))
-               } else {
-                   callback();
-               }
-           };
-
            return {
                registerValidateForm: {
-                   birthday:"2018-07-10T00:00:00.000+08:00",
-                   company: "123",
-                   education: "123",
-                   email: "546052344@qq.com",
-                   gender: '1',
-                   id: 10016,
-                   major: "123",
-                   memberNo: "11111",
-                   memberType: 1,
-                   mobile: "18876335111",
+                   birthday:"",
+                   company: "",
+                   education: "",
+                   email: "",
+                   gender: '',
+                   id: null,
+                   major: "",
+                   memberNo: "",
+                   memberType: null,
+                   mobile: "",
                    name: "",
-                   nationality: "123",
-                   partisan: 1,
-                   reference: 2,
-                   researchField: "123",
-                   title: "123"
+                   nationality: "",
+                   partisan: null,
+                   reference: "",
+                   researchField: "",
+                   title: ""
                },
-
-               rules: {
-                   pass: [
-                       { required: true, validator: validatePass, trigger: ['blur'] }
-                   ],
-                   checkPass: [
-                       { required: true, validator: validatePass2, trigger: ['blur'] }
-                   ],
-                   phone: [
-                       { required: true, validator: validatePhone, trigger: 'change' }
-                   ]
+               required:{
+                   name:"会员姓名",
+                   gender:"会员性别",
+                   school:"学校",
+                   partisan:"党派信息",
+                   company:"工作单位",
+                   title:"职称",
+                   level:"等级",
+                   memberType:"会员类型",
+                   birthday:"出生日期",
+                   major:"所学专业",
+                   workNature:"工作性质",
+                   workajor:"工作专业",
+                   education:"学历",
+                   jobPerformance:"业务成绩",
+                   mobile:"手机号",
+                   zipcode:"邮编",
+                   email:"邮箱",
+                   reference:"推荐单位"
                }
            }
        },
        methods: {
-           submitForm: function(formName) {
-               var _this = this;
+            check: function() {
+                var group = Object.keys(this.required),
+                    meilReq = new RegExp("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$");
 
-               this.$refs[formName].validate((valid) => {
-                   if (valid) {
-                      // _this.submit();
-                   } else {
-                       console.log('error submit!!');
-                       return false;
-                   }
-               });
-           },
-           resetForm: function(formName) {
-               this.$refs[formName].resetFields();
+                if(this.registerValidateForm['email'] && !meilReq.test(this.registerValidateForm['email'])) {
+                    this.$message.error('邮箱格式不正确！');
+                    return
+                }
+
+                for (var i=0; i<group.length; i++) {
+                    var obj = this.registerValidateForm[group[i]];
+
+                    if(!obj || (isNaN(obj) && !obj.length) ) {
+                        this.$message.error('请填写必填字段: '+ this.required[group[i]]);
+                        break;
+                    } else if(i === group.length-1 ) {
+                        this.submit()
+                    }
+
+                }
+
+            },
+           submit: function() {
+                $.ajax({
+                    url:"/member/edit",
+                    type:"post",
+                    data: this.registerValidateForm
+                }).done(function(){
+
+                }.bind(this)).fail(function(){
+
+                }.bind(this))
            }
        },
        mounted: function() {
@@ -88,7 +85,7 @@ $(function() {
            message.gender = message.gender.toString();
            message.memberType = message.memberType.toString();
            message.partisan= message.partisan.toString();
-           message.birthday= new Date(message.birthday);
+           message.birthday= (new Date(message.birthday)).toString();
            this.registerValidateForm = message;
        }
    })
