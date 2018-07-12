c8eb51521b04c79ba549f8a81235ab38c1f9fbae
waonbin <waonbin@126.com>
2018-07-13 00:37:35 +0800

    fix member eidt function

---
+++ mms-manager/src/main/java/com/zlsoft/manager/web/controller/EnrollmentController.java
@@ -0,0 +1,58 @@
+package com.zlsoft.manager.web.controller;
+
+import com.zlsoft.common.service.MeetingEnrollmentService;
+import com.zlsoft.domain.MeetingEnrollment;
+import com.zlsoft.manager.Constants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+@Controller("AdminEnrollmentController")
+@RequestMapping("/meeting/enroll")
+public class EnrollmentController {
+
+    @Inject
+    private MeetingEnrollmentService meetingEnrollmentService;
+
+    /**
+     * GET  / : get meeting enrollment list page
+     * @return meeting enrollment list page
+     */
+    @GetMapping
+    public String getPage() {
+        return "/meeting/enroll/list";
+    }
+
+    /**
+     * GET  /page/{page} : get data of meetings by page
+     * @param page zero-based page index
+     * @return data of meetings by page
+     */
+    @GetMapping("/page/{page}")
+    public @ResponseBody ResponseEntity getMeetings(@PathVariable("page") int page){
+
+        PageRequest pageRequest = PageRequest.of(page, Constants.PAGE_SIZE);
+        Page<MeetingEnrollment> enrollments = this.meetingEnrollmentService.findAll(pageRequest);
+
+        return ResponseEntity.ok(enrollments);
+    }
+
+    /**
+     * GET /detail : get details of meeting enrollment
+     * @param id meeting enrollment id
+     * @return meeting enrollment data
+     */
+    @GetMapping("/details")
+    public @ResponseBody ResponseEntity getMeetingEnrollment(long id){
+        Optional<MeetingEnrollment> enrollment = this.meetingEnrollmentService.findById(id);
+        return ResponseEntity.ok(enrollment);
+    }
+}
--- mms-portal/src/main/java/com/zlsoft/portal/web/controller/MemberController.java
+++ mms-portal/src/main/java/com/zlsoft/portal/web/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.zlsoft.portal.web.controller;
 
+import com.google.common.base.Strings;
 import com.zlsoft.common.service.MemberService;
 import com.zlsoft.domain.Member;
 import com.zlsoft.portal.Constants;
@@ -19,6 +20,7 @@ import javax.servlet.http.HttpSession;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/member")
@@ -140,16 +142,22 @@ public class MemberController extends BaseController {
 
     /**
      * POST  /edit : edit personal information
+     * @param session the HTTP Session
      * @param member member information
      * @return personal information edit page
      * @throws URISyntaxException
      */
     @PostMapping("/edit")
-    public ResponseEntity edit(Member member) {
-        String password = MD5Util.getMD5WithBase64(member.getPassword());
-        member.setPassword(password);
-        member = this.memberService.save(member);
-        return ResponseEntity.ok(member);
+    public ResponseEntity edit(HttpSession session, Member member) {
+        Optional<Member> memberInDB = this.memberService.findById(member.getId());
+
+        if(memberInDB.isPresent()) {
+            member = this.memberService.save(copyFields(memberInDB.get(), member));
+            session.setAttribute(Constants.SESSION_USER, member);
+            return ResponseEntity.ok(member);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     /**
@@ -188,6 +196,39 @@ public class MemberController extends BaseController {
         session.setAttribute(Constants.SESSION_USER, member);
 
         return ResponseEntity.ok().build();
+    }
+
+    private Member copyFields(Member left, Member right) {
+
+        if(!Strings.isNullOrEmpty(right.getEmail())) left.setEmail(right.getEmail());
+        if(!Strings.isNullOrEmpty(right.getName())) left.setName(right.getName());
+        if(right.getMemberType() != null) left.setMemberType(right.getMemberType());
+        if(!Strings.isNullOrEmpty(right.getMobile())) left.setMobile(right.getMobile());
+        if(right.getReference() != null) left.setReference(right.getReference());
+        if(right.getGender() != null) left.setGender(right.getGender());
+        if(right.getBirthday() != null) left.setBirthday(right.getBirthday());
+        if(right.getPartisan() != null) left.setPartisan(right.getPartisan());
+        if(!Strings.isNullOrEmpty(right.getMajor())) left.setMajor(right.getMajor());
+        if(!Strings.isNullOrEmpty(right.getWorkajor())) left.setWorkajor(right.getWorkajor());
+        if(!Strings.isNullOrEmpty(right.getEducation())) left.setEducation(right.getEducation());
+        if(right.getLevel() != null) left.setLevel(right.getLevel());
+        if(!Strings.isNullOrEmpty(right.getWechat())) left.setWechat(right.getWechat());
+        if(!Strings.isNullOrEmpty(right.getOicq())) left.setOicq(right.getOicq());
+        if(!Strings.isNullOrEmpty(right.getFax())) left.setFax(right.getFax());
+        if(!Strings.isNullOrEmpty(right.getTelephone())) left.setTelephone(right.getTelephone());
+        if(!Strings.isNullOrEmpty(right.getZipcode())) left.setZipcode(right.getZipcode());
+        if(right.getProvince() != null) left.setProvince(right.getProvince());
+        if(right.getCity() != null) left.setCity(right.getCity());
+        if(!Strings.isNullOrEmpty(right.getAddress())) left.setAddress(right.getAddress());
+        if(!Strings.isNullOrEmpty(right.getFullAddress())) left.setFullAddress(right.getFullAddress());
+        if(!Strings.isNullOrEmpty(right.getResearchField())) left.setResearchField(right.getResearchField());
+        if(!Strings.isNullOrEmpty(right.getMemberNo())) left.setMemberNo(right.getMemberNo());
+        if(!Strings.isNullOrEmpty(right.getSchool())) left.setSchool(right.getSchool());
+        if(right.getWorkNature() != null) left.setWorkNature(right.getWorkNature());
+        if(!Strings.isNullOrEmpty(right.getJobPerformance())) left.setJobPerformance(right.getJobPerformance());
+        if(right.getExpiryDate() != null) left.setExpiryDate(right.getExpiryDate());
+
+        return left;
     }
 
 }
