854f2310f0038e1dff732ce07c5715ff59215912
zhangwei <546052344@qq.com>
2018-07-18 21:20:02 +0800

    支付页面调整

---
+++ mms-portal/src/main/resources/static/js/src/pay.js
@@ -0,0 +1,33 @@
+$(function() {
+    new Vue({
+        el:"#pay-content",
+        data: {
+            page:1,
+            message: {
+                id:'',
+                name:'111'
+            },
+            list:{}
+        },
+        methods:{
+          getDate: function() {
+              $.ajax({
+                  url:'/payment/page/'+(this.page-1)
+              }).done(function(date) {
+                this.list = date;
+              }.bind(this)).fail(function() {
+                  this.$message.error('提交失败');
+              }.bind(this))
+          },
+          changePage: function(val) {
+              this.page = val;
+              this.getDate()
+          }
+        },
+        mounted: function() {
+            $(".payment-btn").addClass('cur');
+            this.message = message || {name:'---'};
+            this.getDate();
+        }
+    })
+});
