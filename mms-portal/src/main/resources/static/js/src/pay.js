10950b5776535eca4e098eec3e1000cb6e204260
zhangwei <546052344@qq.com>
2018-07-21 22:56:22 +0800

    凭证提交

--- mms-portal/src/main/resources/static/js/src/pay.js
+++ mms-portal/src/main/resources/static/js/src/pay.js
@@ -5,11 +5,25 @@ $(function() {
             page:1,
             message: {
                 id:'',
-                name:'111'
+                name:''
             },
+            payType: '1',
+            step:1,
             list:{}
         },
         methods:{
+          onStep: function(page) {
+              this.step = Number(page)
+          },
+          handlePreview: function() {
+
+          },
+          handleRemove: function() {
+
+          },
+          handExceed: function(file) {
+              this.$message.error('最多只能上传1个文件！');
+          },
           getDate: function() {
               $.ajax({
                   url:'/payment/page/'+(this.page-1)
@@ -31,6 +45,46 @@ $(function() {
           changePage: function(val) {
               this.page = val;
               this.getDate()
+          },
+            beforeUpload(file){
+                let fd = new FormData();
+                fd.append('file',file);//传文件;
+                $.ajax({
+                    url:'/file/upload',
+                    type:"post",
+                    contentType: false,
+                    processData: false,
+                    data:fd
+                })
+                return false
+            },
+          //线下提交
+          underLineSubmit: function() {
+              this.$refs.upload.submit()
+              // this.$refs.upload.submit()
+              // var file = this.$refs.upload.uploadFiles;
+              //
+              // if(file.length === 0) {
+              //     this.$message({
+              //         showClose: true,
+              //         message: '请选择要提交的文件',
+              //         type: 'warning'
+              //     });
+              //     return;
+              // }
+              // var formData = new FormData();
+              // formData.append('file', file[0]);
+              // $.ajax({
+              //     url:'/file/upload',
+              //     type:'post',
+              //     contentType: false,
+              //     processData: false,
+              //     data:formData
+              // }).done(function() {
+              //
+              // }.bind(this)).fail(function() {
+              //     this.$message.error('提交失败！');
+              // }.bind(this))
           }
         },
         mounted: function() {
